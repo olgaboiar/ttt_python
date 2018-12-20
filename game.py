ab71bb422ec1ce29106fc72aec29d0e9f7bd23dd
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-12-20 14:38:59 -0500

    refactored game to be a loop until game over

--- game.py
+++ game.py
@@ -9,18 +9,23 @@ class Game:
     def start(self):
         self.user_interface.greet()
         self.user_interface.print_board(self.board)
-        
+
     def play(self):
         marker1 = self.user_interface.choose_marker()
         player1 = Player(marker1)
         marker2 = player1.define_marker(marker1)
         player2 = Player(marker2)
-        move1 = self.user_interface.choose_move(self.board)
-        player1.move(self.board, move1, marker1)
-        self.user_interface.print_board(self.board)
-        move2 = self.user_interface.choose_move(self.board)
-        player2.move(self.board, move2, marker2)
-        self.user_interface.print_board(self.board)
+        current_player = player1
+        next_player = player2
+        current_marker = marker1
+        next_marker = marker2
+        while not self.game_over(self.board):
+            move = self.user_interface.choose_move(self.board)
+            current_player.move(self.board, move, current_marker)
+            self.user_interface.print_board(self.board)
+            current_player, next_player = next_player, current_player
+            current_marker, next_marker = next_marker, current_marker
+            
 
     def horizontal_win(self, board):
         if board.spots[0] == board.spots[1] == board.spots[2] or board.spots[3] == board.spots[4] == board.spots[5] or board.spots[6] == board.spots[7] == board.spots[8]:
