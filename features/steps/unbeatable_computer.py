c0a09773e09d5b663982b204e9c451eabd5f5007
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-01-02 16:11:19 -0500

    variables names refactoring in behave tests

--- features/steps/unbeatable_computer.py
+++ features/steps/unbeatable_computer.py
@@ -2,23 +2,23 @@ import io
 import sys
 from board import Board
 
-@given('the board is {board}')
-def special_board_state(context, board):
+@given('the board is {raw_spots}')
+def special_board_state(context, raw_spots):
     context.board = Board()
-    context.board.spots = [int(n) if n.isdigit() else n for n in board]
+    context.board.spots = [int(spot) if spot.isdigit() else spot for spot in raw_spots]
 
 @when('human player makes a move on {spot}')
-def human_moves_on_seven(context, spot):
+def human_moves_on_spot(context, spot):
     context.board.insert_value(spot, 'X')
 
-@then('computer makes move on {cell}')
-def board_with_computer_marker_on_eight_is_printed(context, cell):
+@then('computer makes move on {spot}')
+def board_with_computer_marker_on_certain_spot_is_printed(context, spot):
     marker = context.player2.marker
-    spot = context.player2.choose_move(context.board)
-    context.player2.move(context.board, spot, marker)
+    best_move = context.player2.choose_move(context.board)
+    context.player2.move(context.board, best_move, marker)
     captured_output = io.StringIO()
     sys.stdout = captured_output
     context.user_interface.print_board(context.board)
     sys.stdout = sys.__stdout__
     output = captured_output.getvalue()
-    assert cell not in output
+    assert spot not in output
